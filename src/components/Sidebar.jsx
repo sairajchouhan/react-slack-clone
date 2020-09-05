9f8a95182cd0b31bf4606f205341927d292c1a7e
unknown <sairaj2119@gmail.com>
2020-09-05 22:50:05 +0530

    added sidebar and sidebaroptions components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import '../styles/Sidebar.css';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import CreateIcon from '@material-ui/icons/Create';
+import SidebarOption from './SidebarOption';
+// all sidebar option icons
+import InsertCommentIcon from '@material-ui/icons/InsertComment';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
 
 const Sidebar = () => {
-  return <div>sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <div className="sidebar__info">
+          <h2>Team</h2>
+          <h3>
+            <FiberManualRecordIcon />
+            Sairaj
+          </h3>
+        </div>
+        <CreateIcon />
+      </div>
+      <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+      <SidebarOption Icon={InboxIcon} title="Mentions & reactions" />
+      <SidebarOption Icon={DraftsIcon} title="Saved items" />
+      <SidebarOption Icon={BookmarkBorderIcon} title="Channel browser" />
+      <SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+      <SidebarOption Icon={AppsIcon} title="Apps" />
+      <SidebarOption Icon={FileCopyIcon} title="File browser" />
+      <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+      <hr />
+      <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+      <hr />
+      <SidebarOption Icon={AddIcon} title="Add channel" />
+      {/* {connect to database and list all the channels} */}
+    </div>
+  );
 };
 
 export default Sidebar;
---
+++ src/components/SidebarOption.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/SidebarOption.css';
+
+const SidebarOption = ({ Icon, title }) => {
+  return (
+    <div className="sidebarOption">
+      {Icon && <Icon className="sidebarOption__icon" />}
+      {Icon ? (
+        <h3>{title}</h3>
+      ) : (
+        <h3 className="sidebarOption__channel">
+          <span className="sidebarOption__hash">#</span>
+          {title}
+        </h3>
+      )}
+    </div>
+  );
+};
+
+export default SidebarOption;
